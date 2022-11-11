b2a76d74f0fbaec8a67b8e6de0344c54359e3679
NicholasPeara92 <nicholas.peara@gmail.com>
2022-11-11 14:32:07 +0100

    Ciao

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-createApp({
+const { createApp } = Vue
+
+app = createApp({
     data() {
       return {
-        message: 'Hello'
-        avatar: 'https://dictionary.cambridge.org/it/images/thumb/clown_noun_002_07246.jpg?version=5.0.273'
+        message: 'Hello',
+        image: 'https://dictionary.cambridge.org/it/images/thumb/clown_noun_002_07246.jpg?version=5.0.273',
       }
     }
-  }).mount('#app')
-</script>
+}).mount('#app')
