cf20726236dd5b52c5878a21959eb502c217116c
NicholasPeara92 <nicholas.peara@gmail.com>
2022-11-11 14:09:08 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+createApp({
+    data() {
+      return {
+        message: 'Hello'
+        avatar: 'https://dictionary.cambridge.org/it/images/thumb/clown_noun_002_07246.jpg?version=5.0.273'
+      }
+    }
+  }).mount('#app')
+</script>
